e501c196bde57002ba633b08bb54abc686203374
JHONATANGARCIAXD <jhonatangarcia@colegioguaneta.edu.co>
2023-02-15 06:24:34 -0500

    se agrega el programa conversor_temperatura

---
+++ conversor_temperatura.py
@@ -0,0 +1,13 @@
+# conversor de temperatura
+
+# intput 
+
+c = int(input("digite el numero c: "))
+
+#processing 
+
+F = (c*1.8 + 32)
+k = (c+273.15)
+
+print("la convercion de farenhell en " + str(F))
+print("la convercion de kelvin es " + str (k) )
